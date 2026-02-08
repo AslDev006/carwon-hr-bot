bbd3f63622f5a99fe51dfcf2ee2126b46648aa3b
AslDev006 <asldev006@gmail.com>
2026-02-08 15:07:58 +0500

    Fixed a apps

--- models/apps.py
+++ models/apps.py
@@ -1,5 +1,5 @@
-import threading
 import os
+import threading
 from django.apps import AppConfig
 
 class ModelsConfig(AppConfig):
@@ -7,6 +7,6 @@ class ModelsConfig(AppConfig):
     name = 'models'
 
     def ready(self):
-        if os.environ.get('RUN_MAIN') == 'true':
+        if os.environ.get('RUN_MAIN') == 'true' or os.environ.get('GUNICORN_RUN') == 'true':
             from .bot_setup import start_bot
             threading.Thread(target=start_bot, daemon=True).start()
